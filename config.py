219f0abc0ae60cfafbd34771107bfb3f022d2642
itbhat <vineetvbhat@yahoo.com>
2022-07-10 21:44:43 +0530

    error handling final

--- config.py
+++ config.py
@@ -8,4 +8,11 @@ class Config(object):
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   UPLOAD_FOLDER = os.path.join(basedir, 'app.db') + '/app'
   ALLOWED_EXTENSIONS = {'.csv'}
+  MAIL_SERVER = os.environ.get('MAIL_SERVER')
+  MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+  MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+  MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+  MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+  ADMINS = ['vineet99bhat@gmail.com']
+
   
